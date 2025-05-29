e45fdf41db94ecc9c727d9463560e98027db6851
Ryless KECHIT <ryless.kechit@outlook.com>
2025-05-29 21:59:09 +0200

    changed number

--- pages/index.js
+++ pages/index.js
@@ -204,13 +204,13 @@ export default function CEOProfile() {
                 </a>
 
                 <a 
-                  href="tel:+971506195207" 
+                  href="tel:+971565663377" 
                   className="flex items-center justify-center gap-3 text-gray-200 hover:text-accent transition-colors p-3 rounded-lg hover:bg-dark-hover"
                 >
                   <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
                   </svg>
-                  <span>🇦🇪 +971 50 619 5207</span>
+                  <span>🇦🇪 +971 56 566 3377</span>
                 </a>
               </div>
             </div>
